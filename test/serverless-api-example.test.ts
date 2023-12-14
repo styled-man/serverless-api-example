91d34e8528cc343c41c9ee9c4859408e1446763c
Smyvens Estime <smyvensestime@gmail.com>
2023-12-14 07:46:47 -0500

    Initial commit

---
+++ test/serverless-api-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessApiExample from '../lib/serverless-api-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-api-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessApiExample.ServerlessApiExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
